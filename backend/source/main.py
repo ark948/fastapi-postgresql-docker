46baee37570665cab24c4788c461a0bf7f7de0f9
ark948 <armankarimi948@gmail.com>
2025-01-06 22:35:37 +0330

    fastapi docker 010

--- backend/source/main.py
+++ backend/source/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Depends, Body
 from contextlib import asynccontextmanager
-from source.database import init_db
+from source.sections.database.provider import init_db, get_async_session
+from source.sections.redis import get_redis
+from sqlalchemy import text
 from source.config import Config
 
 
@@ -23,7 +26,7 @@ app = FastAPI(
 @app.get('')
 @app.get('/')
 def root():
-    return "async databas inside dockere"
+    return "async databas inside docker"
 
 
 
@@ -36,3 +39,38 @@ def test():
 @app.get('/read-env')
 def read_env():
     return Config.SECRET_KEY
+
+
+
+@app.get('/test-db')
+async def test_db_connection(db = Depends(get_async_session)):
+    try:
+        stmt = text("SELECT 'Hello From DB;'")
+        result = await db.execute(stmt)
+        result = result.all()
+        print("\n\n", result, "\n\n")
+        return "OK"
+    except Exception as error:
+        return "NOT OK"
+
+
+
+@app.post('/test-redis')
+async def test_redis_post(
+    name: Annotated[str, Body(embed=True)], 
+    value: Annotated[str, Body(embed=True)], 
+    redis_client = Depends(get_redis)
+    ):
+    try:
+        await redis_client.set(name=name, value=value, ex=3600)
+        return "OK"
+    except Exception as error:
+        return "ERROR"
+    
+
+
+@app.get('/test-redis/{name}')
+async def test_redis_get(name: str, redis_client = Depends(get_redis)):
+    result = await redis_client.get(name)
+    return result
+    
---
+++ backend/source/sections/database/provider.py
@@ -0,0 +1,39 @@
+from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlmodel import create_engine, text
+from sqlmodel.ext.asyncio.session import AsyncSession
+from typing import AsyncGenerator
+from sqlalchemy.orm import sessionmaker
+
+
+from source.config import Config
+
+
+async_engine = AsyncEngine(
+    create_engine(url=Config.DB_URL)
+)
+
+
+async def init_db():
+    async with async_engine.begin() as conn:
+        statement = text("SELECT 'DATABASE CONNECTED';")
+        result = await conn.execute(statement)
+        print(result.all())
+
+
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    Session = sessionmaker(
+        bind=async_engine,
+        class_=AsyncSession,
+        expire_on_commit=False
+    )
+
+    async with Session() as session:
+        try:
+            yield session
+        except:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
